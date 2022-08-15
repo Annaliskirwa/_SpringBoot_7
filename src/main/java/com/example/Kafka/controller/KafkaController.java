267194ae5b48f652e359368396835c5818f89f77
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-15 23:11:31 +0300

    injecting

--- src/main/java/com/example/Kafka/controller/KafkaController.java
+++ src/main/java/com/example/Kafka/controller/KafkaController.java
@@ -1,6 +1,9 @@
 package com.example.Kafka.controller;
 
 import com.example.Kafka.model.KafkaModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/kafka")
 public class KafkaController {
+    private KafkaTemplate<String, KafkaModel> kafkaTemplate;
+    @Autowired
+    public KafkaController(KafkaTemplate<String, KafkaModel> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    @PostMapping
     public void post(@RequestBody KafkaModel kafkaModel){
-
+        kafkaTemplate.send("myTopic", kafkaModel);
     }
 }
