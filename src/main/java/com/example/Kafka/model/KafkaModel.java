180eb82d5103d0a07a7f3aec13c2c2ccb1a91c83
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-15 22:54:30 +0300

    initi project for kafka prod cons demo

---
+++ src/main/java/com/example/Kafka/model/KafkaModel.java
@@ -0,0 +1,15 @@
+package com.example.Kafka.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaModel {
+    private String field1;
+    private String field2;
+}
