8363617dc45d850ab36fbefd84ed60f8353dec77
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-15 23:04:12 +0300

    configured kafka

--- src/main/java/com/example/Kafka/controller/KafkaController.java
+++ src/main/java/com/example/Kafka/controller/KafkaController.java
@@ -1,9 +1,14 @@
 package com.example.Kafka.controller;
 
+import com.example.Kafka.model.KafkaModel;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/kafka")
 public class KafkaController {
+    public void post(@RequestBody KafkaModel kafkaModel){
+
+    }
 }
