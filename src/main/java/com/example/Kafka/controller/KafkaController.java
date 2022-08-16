8a9bfb7f93302fade8092820e895bd6a3b829349
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-16 13:06:22 +0300

    json converter

--- src/main/java/com/example/Kafka/controller/KafkaController.java
+++ src/main/java/com/example/Kafka/controller/KafkaController.java
@@ -1,6 +1,7 @@
 package com.example.Kafka.controller;
 
 import com.example.Kafka.model.KafkaModel;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,22 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/kafka")
 public class KafkaController {
-    private KafkaTemplate<String, KafkaModel> kafkaTemplate;
+//    private KafkaTemplate<String, KafkaModel> kafkaTemplate;
+private KafkaTemplate<String, String> kafkaTemplate;
+private Gson jsonConverter;
+
     @Autowired
-    public KafkaController(KafkaTemplate<String, KafkaModel> kafkaTemplate){
+    public KafkaController(KafkaTemplate<String, String> kafkaTemplate, Gson jsonConverter){
         this.kafkaTemplate = kafkaTemplate;
+        this.jsonConverter = jsonConverter;
     }
     @PostMapping
     public KafkaModel post(@RequestBody KafkaModel kafkaModel){
-        kafkaTemplate.send("myTopic", kafkaModel);
+        kafkaTemplate.send("myTopic", jsonConverter.toJson(kafkaModel));
         return kafkaModel;
 //        This kafka model is returned in the kafka consumer console
     }
 
     @KafkaListener(topics="myTopic")
-    public void getFromKafka(KafkaModel kafkaModel){
+    public void getFromKafka(String kafkaModel){
 //        System.out.println(kafkaModel.toString());
         log.info("*********Get From Kafka**************");
-        log.info(kafkaModel.getField1() + kafkaModel.getField2());
+        log.info(kafkaModel);
+
+        KafkaModel kafkaModel1 = (KafkaModel) jsonConverter.fromJson(kafkaModel, KafkaModel.class);
+        log.info("**********After json converter***********");
+        log.info(kafkaModel1.getField1()+kafkaModel1.getField2());
     }
 }
