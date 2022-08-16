6fce83d0d0dd5ada57d7e30ad8096f3971b67dde
Annaliskirwa <annaliskirwa@gmail.com>
2022-08-16 14:33:53 +0300

    new model, kafka listener to that topic

--- src/main/java/com/example/Kafka/controller/KafkaController.java
+++ src/main/java/com/example/Kafka/controller/KafkaController.java
@@ -1,6 +1,7 @@
 package com.example.Kafka.controller;
 
 import com.example.Kafka.model.KafkaModel;
+import com.example.Kafka.model.KafkaModel2;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,17 +29,31 @@ private Gson jsonConverter;
     public KafkaModel post(@RequestBody KafkaModel kafkaModel){
         kafkaTemplate.send("myTopic", jsonConverter.toJson(kafkaModel));
         return kafkaModel;
-//        This kafka model is returned in the kafka consumer console
+    }
+
+    @PostMapping("/v2")
+    public KafkaModel2 post(@RequestBody KafkaModel2 kafkaModel2){
+        kafkaTemplate.send("myTopic2", jsonConverter.toJson(kafkaModel2));
+        return kafkaModel2;
     }
 
     @KafkaListener(topics="myTopic")
     public void getFromKafka(String kafkaModel){
-//        System.out.println(kafkaModel.toString());
         log.info("*********Get From Kafka**************");
         log.info(kafkaModel);
 
         KafkaModel kafkaModel1 = (KafkaModel) jsonConverter.fromJson(kafkaModel, KafkaModel.class);
         log.info("**********After json converter***********");
         log.info(kafkaModel1.getField1()+kafkaModel1.getField2());
+    }
+
+    @KafkaListener(topics = "myTopic2")
+    public void getFromKafka2(String kafkaModel2){
+        log.info("*********Get From Kafka**************");
+        log.info(kafkaModel2);
+
+        KafkaModel2 kafkaModel = (KafkaModel2) jsonConverter.fromJson(kafkaModel2, KafkaModel2.class);
+        log.info("**********After json converter***********");
+        log.info(kafkaModel.getTitle() + kafkaModel.getDescription());
     }
 }
